c71f549dd2b3327389c20d472cf2dc7d057e5095
Shun Sakai <sorairolake@protonmail.ch>
2024-07-13 23:23:46 +0900

    Initialize Cargo package

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2024 Shun Sakai
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! The `nt-time` crate is a library for testing whether a given data is a [SVG]
+//! image.
+//!
+//! # Examples
+//!
+//! ```
+//! ```
+//!
+//! [SVG]: https://www.w3.org/Graphics/SVG/
+
+#![doc(html_root_url = "https://docs.rs/is-svg/0.1.0/")]
+// Lint levels of rustc.
+#![forbid(unsafe_code)]
+#![deny(missing_debug_implementations, missing_docs)]
+#![warn(rust_2018_idioms)]
+// Lint levels of Clippy.
+#![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
